40bf4a525a0d188221c71efa77c3751f20b44051
Isak Jung <isack19999@gmail.com>
2023-03-26 23:49:01 +0900

    add

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -35,10 +35,17 @@ class MemberServiceTest {
     @Test
     public void 중복_회원_예외() {
         //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
 
         //when
+        memberService.join(member1);
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
         //then
-
+//        fail("예외가 발생해야 한다.");
     }
 }
