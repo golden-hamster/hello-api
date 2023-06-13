81c68f5ab587d7fa2988657d7e8472c896542260
golden-hamster <isack19999@gmail.com>
2023-06-13 14:09:18 +0900

    hello-api

--- src/main/java/jpabook/jpashop/JpaShopApplication.java
+++ src/main/java/jpabook/jpashop/JpaShopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaShopApplication {
@@ -9,5 +11,6 @@ public class JpaShopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JpaShopApplication.class, args);
 	}
+
 
 }
